2390d9350ab707b5d2abd7a3ce14a570e0d2f9be
thomas allen <thomasallenmartinho@gmail.com>
2023-09-20 16:48:40 +0200

    edited recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
   has_one_attached :photo
+  validates :title, :description, :servings, :category, presence: true
+  validates :prep_time, :rate, :servings, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
