50f1e355ea020fad5e9eaaedc5ba0704773a243b
thomas allen <thomasallenmartinho@gmail.com>
2023-09-28 16:47:36 +0200

    finally solved this db issue

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   has_one_attached :photo
   validates :title, :description, :servings, :category, presence: true
-  validates :prep_time, :rate, :servings, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :prep_time, :servings, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
   def photo_url
     if photo.attached?
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,15 @@ Recipe.destroy_all
 puts 'Creating recipes'
 
 50.times do |i|
-  Recipe.create(
+  recipe = Recipe.new(
     title: "Recipe #{i + 1}",
     description: 'This is a great recipe',
     rate: [8, 9, 10].sample.to_f,
-    prep_time: [30, 45, 60].sample,
-    servings: [2, 4, 6].sample,
+    prep_time: [30, 45, 60].sample.to_i,
+    servings: [2, 4, 6].sample.to_i,
     category: 'Best category'
   )
+  recipe.save!
   puts "Recipe #{i + 1} done!"
 end
 
