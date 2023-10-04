3404b58d7df8b24e5c3becc7bff8c95cd3dadb60
thomas allen <thomasallenmartinho@gmail.com>
2023-10-04 18:51:48 +0200

    it is fixed oh yeah

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,11 +4,11 @@ class Api::V1::RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.order(created_at: :desc)
-    render json: @recipes, methods: [:photo_key]
+    render json: @recipes
   end
 
   def show
-    render json: @recipe, methods: [:photo_key]
+    render json: @recipe
   end
 
   def create
@@ -36,7 +36,7 @@ class Api::V1::RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :prep_time, :rate, :servings, :category)
+    params.require(:recipe).permit(:title, :description, :prep_time, :rate, :servings, :category, :photo_url)
   end
 
   def set_recipe
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_28_143552) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_161555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_28_143552) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
